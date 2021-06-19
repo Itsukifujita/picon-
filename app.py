61e17b5808e7c86f3118856d8bacb48a7184ff49
fujita <fujisan1397@icloud.com>
2021-06-19 16:36:02 +0900

    hello-heroku-flask

--- app.py
+++ app.py
@@ -11,20 +11,20 @@ line_bot_api = LineBotApi(ACCESS_TOKEN)
 handler = WebhookHandler(SECRET)
 
 
-@app.route(“/callback”, methods=[‘POST’])
+@app.route("/callback", methods=['POST'])
 def callback():
-    signature = request.headers[‘X-Line-Signature’]
+    signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
-    app.logger.info(“Request body: ” + body)
+    app.logger.info("Request body: " + body)
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-    return ‘OK’
+    return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
