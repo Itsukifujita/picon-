9a88e2c06e44cdfbab3bae4b1a2c9542007cbdb0
fujita <fujisan1397@icloud.com>
2021-06-26 13:25:58 +0900

    abababab

--- static/js/VIEW.js
+++ static/js/VIEW.js
@@ -7,49 +7,19 @@ const JSVIEW = {
             len = res.length;
         for (let i = 0; i < len; i++) {
             html += `<li class="task" data-id="${res[i][0]}">`
-                  +     `<input type="text" name="task_name" class="task_detail">`
+                  +     `<input type="text" name="task_name" class="task_detail" value="${res[i][1]}">`
                   +     `<input type="date" class="task_date">`
                   +     JSVIEW.createSelectDay()
-                  +     `<option value="rei">00</option>`
-               + `<option value="ich">01</option>`
-               + `<option value="nii">02</option>`
-               + `<option value="san">03</option>`
-               + `<option value="sii">04</option>`
-            + `<option value="goo">05</option>`
-            + `<option value="rok">06</option>`
-            + `<option value="sit">07</option>`
-            + `<option value="hat">08</option>`
-            + `<option value="kuu">09</option>`
-            + `<option value="juu">10</option>`
-            + `<option value="jui">11</option>`
-            + `<option value="jun">12</option>`
-            + `<option value="jus">13</option>`
-            + `<option value="juy">14</option>`
-            + `<option value="jug">15</option>`
-            + `<option value="jur">16</option>`
-            + `<option value="jna">17</option>`
-            + `<option value="juh">18</option>`
-            + `<option value="juk">19</option>`
-            + `<option value="nij">20</option>`
-            + `<option value="nji">21</option>`
-            + `<option value="njn">22</option>`
-            + `<option value="njs">23</option>`
-            + `<option value="njy">24</option>`
-            + `</select>` 
-            + `<select>`
-            + `<option value="zer">00</option>`
-            + `<option value="qua">15</option>`
-            + `<option value="hal">30</option>`
-            + `<option value="thr">45</option>`
-            + `</select>`;
-        
-            $('#task_list').html('<li class="task"><input type="text" class="task_content"></li>')
+                  +     JSVIEW.createSelectHour()
+                  +     JSVIEW.createSelectMinuites()
+                  + `</li>`;
         }
+        $('#task_list').html(html);
     },
     createNoTask: function() {
         
     },
-    createSelectDay: function() {
+    createSelectDay: function(day) {
         let html = `<select class="task_select_day">`,
             array = ['毎日', '毎週月曜日', '毎週火曜日', '毎週水曜日', '毎週木曜日', '毎週金曜日', '毎週土曜日', '毎週日曜日'];
         for (let i in array) {
@@ -58,10 +28,10 @@ const JSVIEW = {
         html += `</select>`;
         return html;
     },
-    createSelectHour: function() {
+    createSelectHour: function(time) {
         
     },
-    createSelectMinuites: function() {
+    createSelectMinuites: function(time) {
         
     }
 };
