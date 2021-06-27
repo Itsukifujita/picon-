33779a28f6e151bd9626ff85f3d6235a8264cc07
fujita <fujisan1397@icloud.com>
2021-06-27 20:40:07 +0900

    aaa

--- app.py
+++ app.py
@@ -122,7 +122,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    mes = str(event.message.text) + "www"
+    mes = str(event.message.text) + str(event.reply_token)
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=mes))
 
 if __name__ == "__main__":
